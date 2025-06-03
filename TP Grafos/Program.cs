b4d7561a0d06564c06814e9c85d90fa744a4d45e
Luis <luisgustavomoura45@gmail.com>
2025-06-03 17:15:38 -0300

    Adicionar arquivos de projeto.

---
+++ TP Grafos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Aresta
+{
+    public int Inicio;
+    public int Fim;
+    public double Peso;
+    public Aresta(int Inicio, int Fim, double Peso)
+    {
+        this.Inicio = Inicio;
+        this.Fim = Fim;
+        this.Peso = Peso;
+    }
+
+    public override string ToString()
+    {
+        return $"{Inicio} -- {Peso} -> {Fim}";
+    }
+}
+class Grafo
+{
+    public int quantVertices;
+    public int quantArestas;
+    public double DensidadeGrafo = 0;
+
+    public double[,] matrizAdjacencia;
+    public List<Aresta>;
+
+    public double CalcDensidade(int quantVertices, int quantArestas)
+    {
+        DensidadeGrafo = quantArestas / (quantVertices * (quantVertices - 1));
+        return DensidadeGrafo;
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
